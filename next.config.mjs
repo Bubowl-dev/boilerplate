834cc167ecbd9cbb467c29bd8034a27d54e1b340
Gustavo pontes <gustavo pontes>
2024-08-27 20:53:03 -0300

    feat: script and componente lazy img

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: false,
+  compiler: {
+    styledComponents: true,
+  },
+  trailingSlash: true,
+  output: "export",
+  distDir: "build",
+};
 
 export default nextConfig;
